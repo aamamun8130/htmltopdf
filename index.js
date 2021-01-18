82443fe280aba960ea34061f2ca5aa7066c3c1cd
aamamun8130 <md.abdullal8130@gmail.com>
2021-01-17 18:30:11 -0800

    Added: Handlebars template to PDF generation

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs-extra");
+const hbs = require("handlebars");
+const path = require("path")
+
+const data = require("./database.json");
 
 let htmlString = '<button>Submit</button>';
+
+const compile = async function(templateName, data){
+    const filePath = path.join(process.cwd(), 'templates', `${templateName}.hbs`);
+    const html = await fs.readFile(filePath, 'utf-8');
+    return hbs.compile(html)(data);
+};
 
 (async function(){
     try{
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
-        await page.setContent(htmlString);
+        const content = await compile('pdfhtmltemplate', data)
+        await page.setContent(content);
        // await page.emulateMedia('screen');
         await page.pdf({
             path: 'htmlpdf.pdf',
